98a87614b0556885b029c388f99cdcc26ea3a738
Dmitrii Kuzmin <kd@kode.ru>
2021-07-04 18:02:23 +0300

    fix: Changed default styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert/alert.component';
+import { AlertService } from './alert/alert.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, AlertComponent],
+  imports: [BrowserModule],
+  providers: [AlertService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
